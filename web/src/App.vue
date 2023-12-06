01deaded32a9db344d6720ff386769e1ec96f852
ziyang wu <wuziyangzy@163.com>
2023-12-06 15:15:51 +0800

    项目骨架搭建好了

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,43 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div>
+    <!-- setup中return了在这里就可以直接调用了{{。。。}}两个大括号调用-->
+    <div>Bot昵称：{{ bot_name }}</div>  
+    <div>Bot战力：{{ bot_rating }}</div>
+  </div>
   <router-view/>
 </template>
 
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';  //定义两个变量：昵称和战力。vue里定义变量需要ref
+
+export default {
+  name: "App",
+  setup: () => {  //setup是整个函数的入口
+    let bot_name = ref("");
+    let bot_rating = ref("");
+
+    //访问后端连接，可以用ajax写
+    $.ajax({
+      url: "http://127.0.0.1:3000/pk/getbotinfo/",  //url是我们的函数地址
+      type: "get",  //请求就两种get获取数据，post是创建数据
+      success: resp => {
+        bot_name.value = resp.name;
+        bot_rating.value = resp.rating;
+      }
+    });
+
+    return {
+      bot_name,
+      bot_rating
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  background-image: url("@/assets/background.png");
+  background-size: cover;   /* //100%填充图片 */
 }
 </style>
